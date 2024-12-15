2f35f189807311294e40a077c1579956e37918f2
void2610 <izumisan2610@gmail.com>
2024-12-15 19:03:40 +0900

    add effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,53 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem gun;
+    [SerializeField] private GameObject explosionPrefab;
+    private List<ParticleCollisionEvent> collisionEvents = new();
     private Rigidbody2D rb;
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        gun.GetCollisionEvents(other, collisionEvents);
+
+        var intersection = collisionEvents[0].intersection;
+        var explosion = Instantiate(explosionPrefab, intersection, Quaternion.identity);
+
+        var p = explosion.GetComponent<ParticleSystem>();
+        p.Play();
+
+        if (other.GetComponent<Rigidbody2D>() != null)
+        {
+            Debug.Log("Adding force");
+            other.GetComponent<Rigidbody2D>()
+                .AddForceAtPosition((intersection * 100) - transform.position, intersection + Vector3.up);
+        }
+        
+    }
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gun.Play();
+    }
+
+    private void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space)) gun.Play();
+        // else if (Input.GetKeyUp(KeyCode.Space)) gun.Stop();
     }
 
     private void FixedUpdate()
     {
         var move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(move * 10, rb.velocity.y);
+        rb.velocity = new Vector2(move * 13, rb.velocity.y);
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * 250);
+            rb.AddForce(Vector2.up * 500);
         }
     }
 }
