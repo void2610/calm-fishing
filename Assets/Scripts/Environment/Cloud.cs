13bffb545ee97005716160ebdea299f92e822e15
void2610 <izumisan2610@gmail.com>
2024-12-28 19:16:23 +0900

    マウスで雲を削除

--- Assets/Scripts/Environment/Cloud.cs
+++ Assets/Scripts/Environment/Cloud.cs
@@ -12,25 +12,55 @@ namespace Environment
     {
 
         [SerializeField] private RainCollider rain;
+
+        private float _lifeTime = float.PositiveInfinity;
+        private bool _isStopping = false;
         
-        public void Init(Vector3 pos, InteractableWater water, float lifeTime = -1)
+        public void Init(Vector3 pos, InteractableWater water, float lifeTime)
         {
             rain.Init(water);
             transform.DOMove(pos, 3f).SetEase(Ease.InOutSine).SetLink(gameObject);
-            
-            if (lifeTime > 0)
-            {
-                StartCoroutine(StopCoroutine(lifeTime));
-            }
+            _lifeTime = lifeTime;
+        }
+        
+        public void SubtractLifeTime(float time)
+        {
+            _lifeTime -= time;
+            if(_lifeTime <= 0)
+                StartCoroutine(nameof(StopCoroutine), 1.0f);
         }
         
         private IEnumerator StopCoroutine(float time)
         {
-            yield return new WaitForSeconds(time);
+            Debug.Log("Stop");
+
+            _isStopping = true;
             rain.StopRain();
             GetComponent<VisualEffect>().Stop();
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(time);
             Destroy(gameObject);
+        }
+
+        private void Update()
+        {
+            if(_isStopping) return;
+            
+            if(_lifeTime > 0)
+                _lifeTime -= Time.deltaTime;
+            else
+            {
+                StartCoroutine(nameof(StopCoroutine), 1.0f);
+            }
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log(other.name);
+            if (other.TryGetComponent(out MouseCollider mouseCollider))
+            {   
+                _lifeTime -= 0.5f;
+                Debug.Log("Hit");
+            }
         }
     }
 }
